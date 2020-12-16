a6dedde535eb2f9687f3db064fba628147738ad2
Jed Fox <jed@neeva.co>
2020-12-16 15:42:13 -0500

    Add demo/testing app

---
+++ Demo App/Neeva iOS Support/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  Neeva iOS Support
+//
+//  Created by Jed Fox on 12/16/20.
+//
+
+import UIKit
+import NeevaSupport
+import SwiftKeychainWrapper
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tokenInput: UITextField!
+    @IBOutlet weak var dataOutput: UILabel!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tokenInput.text = KeychainWrapper.standard.string(forKey: loginKeychainKey)
+    }
+
+    @IBAction func runUserQuery(_ sender: Any) {
+        KeychainWrapper.standard.set(tokenInput.text!, forKey: loginKeychainKey)
+        activityIndicator.startAnimating()
+        UIView.animate(withDuration: 0.4) {
+            self.activityIndicator.alpha = 1
+        }
+        GraphQLAPI.fetch(query: UserInfoQuery()) { result in
+            self.activityIndicator.stopAnimating()
+            UIView.animate(withDuration: 0.4) {
+                self.activityIndicator.alpha = 0
+            }
+            switch result {
+            case .success(let result):
+                if let errors = result.errors, !errors.isEmpty {
+                    let messages = errors.filter({ $0.message != nil }).map({ $0.message! })
+                    self.dataOutput.text = "ERROR:\n\(messages.joined(separator: "\n"))"
+                } else if let user = result.data?.user {
+                    self.dataOutput.text = "Hello, \(user.profile.displayName)!\n" + String(
+                        data: try! JSONSerialization.data(withJSONObject: user.jsonObject, options: [.prettyPrinted, .sortedKeys]),
+                        encoding: .utf8
+                    )!
+                } 
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
+
