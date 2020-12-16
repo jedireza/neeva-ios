0331dc84204c3c393caa920352668920305dc722
Jed Fox <jed@neeva.co>
2020-12-16 14:00:59 -0500

    Remove tests

--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import neeva_ios_supportTests
-
-var tests = [XCTestCaseEntry]()
-tests += neeva_ios_supportTests.allTests()
-XCTMain(tests)
--- Tests/neeva-ios-supportTests/XCTestManifests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-
-#if !canImport(ObjectiveC)
-public func allTests() -> [XCTestCaseEntry] {
-    return [
-        testCase(neeva_ios_supportTests.allTests),
-    ]
-}
-#endif
