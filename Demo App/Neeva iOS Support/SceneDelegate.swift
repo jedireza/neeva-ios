197685093dc6edd8c1423b7511e8c30de0729f26
Jed Fox <jed@neeva.co>
2020-12-16 15:47:13 -0500

    Clean up & fix code

---
+++ Demo App/Neeva iOS Support/SceneDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  SceneDelegate.swift
+//  Neeva iOS Support
+//
+//  Created by Jed Fox on 12/16/20.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+}
+
--- Demo App/Neeva iOS Support/ViewController.swift
+++ Demo App/Neeva iOS Support/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var tokenInput: UITextField!
     @IBOutlet weak var dataOutput: UILabel!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         tokenInput.text = KeychainWrapper.standard.string(forKey: loginKeychainKey)
