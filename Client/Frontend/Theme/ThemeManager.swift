2f7eda9f4c8b2e9f8df686c59640a7a56264ad92
Darin Fisher <79867437+darinwf@users.noreply.github.com>
2021-04-28 11:34:11 -0700

    Remove unnecessary persistence of theme configuration (#244)
    This also ensures that anyone who had set their theme using the old settings
    will now have system dark mode settings applied.

--- Client/Frontend/Theme/ThemeManager.swift
+++ Client/Frontend/Theme/ThemeManager.swift
@@ -3,19 +3,11 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 import Foundation
 
-enum ThemeManagerPrefs: String {
-    case systemThemeIsOn = "prefKeySystemThemeSwitchOnOff"
-    case automaticSwitchIsOn = "prefKeyAutomaticSwitchOnOff"
-    case automaticSliderValue = "prefKeyAutomaticSliderValue"
-    case themeName = "prefKeyThemeName"
-}
-
 class ThemeManager {
     static let instance = ThemeManager()
 
-    var current: Theme = themeFrom(name: UserDefaults.standard.string(forKey: ThemeManagerPrefs.themeName.rawValue)) {
+    var current: Theme = NormalTheme() {
         didSet {
-            UserDefaults.standard.set(current.name, forKey: ThemeManagerPrefs.themeName.rawValue)
             NotificationCenter.default.post(name: .DisplayThemeChanged, object: nil)
         }
     }
@@ -24,31 +16,7 @@ class ThemeManager {
         return BuiltinThemeName(rawValue: ThemeManager.instance.current.name) ?? .normal
     }
 
-    var automaticBrightnessValue: Float = UserDefaults.standard.float(forKey: ThemeManagerPrefs.automaticSliderValue.rawValue) {
-        didSet {
-            UserDefaults.standard.set(automaticBrightnessValue, forKey: ThemeManagerPrefs.automaticSliderValue.rawValue)
-        }
-    }
-
-    var automaticBrightnessIsOn: Bool = UserDefaults.standard.bool(forKey: ThemeManagerPrefs.automaticSwitchIsOn.rawValue) {
-        didSet {
-            UserDefaults.standard.set(automaticBrightnessIsOn, forKey: ThemeManagerPrefs.automaticSwitchIsOn.rawValue)
-            updateCurrentThemeBasedOnScreenBrightness()
-        }
-    }
-
-    var systemThemeIsOn: Bool {
-        didSet {
-            UserDefaults.standard.set(systemThemeIsOn, forKey: ThemeManagerPrefs.systemThemeIsOn.rawValue)
-        }
-    }
-
-    private init() {
-        UserDefaults.standard.register(defaults: [ThemeManagerPrefs.systemThemeIsOn.rawValue: true])
-        systemThemeIsOn = UserDefaults.standard.bool(forKey: ThemeManagerPrefs.systemThemeIsOn.rawValue)
-
-        NotificationCenter.default.addObserver(self, selector: #selector(brightnessChanged), name: UIScreen.brightnessDidChangeNotification, object: nil)
-    }
+    let systemThemeIsOn: Bool = true
 
     // UIViewControllers / UINavigationControllers need to have `preferredStatusBarStyle` and call this.
     var statusBarStyle: UIStatusBarStyle {
@@ -57,7 +25,6 @@ class ThemeManager {
                 return .darkContent
             }
         }
-
         return currentName == .dark ? .lightContent : .default
     }
 
@@ -69,23 +36,6 @@ class ThemeManager {
         default:
             return .light
         }
-    }
-
-    func updateCurrentThemeBasedOnScreenBrightness() {
-        let prefValue = UserDefaults.standard.float(forKey: ThemeManagerPrefs.automaticSliderValue.rawValue)
-
-        let screenLessThanPref = Float(UIScreen.main.brightness) < prefValue
-
-        if screenLessThanPref, self.currentName == .normal {
-            self.current = DarkTheme()
-        } else if !screenLessThanPref, self.currentName == .dark {
-            self.current = NormalTheme()
-        }
-    }
-
-    @objc private func brightnessChanged() {
-        guard automaticBrightnessIsOn else { return }
-        updateCurrentThemeBasedOnScreenBrightness()
     }
 }
 
