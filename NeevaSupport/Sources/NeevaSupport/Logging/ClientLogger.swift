c6f38acaa75e9a28588ae6ce1357a13f77ac8e62
macy-n <61707598+macy-n@users.noreply.github.com>
2021-05-10 11:35:03 -0700

    [ios] disable client logging (#288)
    * [ios] disable client logging

--- NeevaSupport/Sources/NeevaSupport/Logging/ClientLogger.swift
+++ NeevaSupport/Sources/NeevaSupport/Logging/ClientLogger.swift
@@ -8,16 +8,28 @@
 import Foundation
 import Apollo
 
+enum ClientLoggerStatus {
+    case enabled
+    case disabled
+}
+
 public class ClientLogger {
     public var env: ClientLogEnvironment
+    private let status: ClientLoggerStatus
 
     public static let shared = ClientLogger()
 
     public init() {
         self.env = ClientLogEnvironment.init(rawValue: "Prod")!
+        // disable client logging until we have a plan for privacy control
+        self.status = .disabled
     }
 
     public func logCounter(_ path: LogConfig.Interaction, attributes: [ClientLogCounterAttribute] = []) {
+        if self.status != ClientLoggerStatus.enabled {
+            return
+        }
+
         let clientLogBase = ClientLogBase(id: "co.neeva.app.ios.browser", version: Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String, environment: self.env)
         let clientLogCounter = ClientLogCounter(path: path.rawValue, attributes: attributes)
         let clientLog = ClientLog(counter: clientLogCounter)
@@ -34,5 +46,6 @@ public class ClientLogger {
                 break
             }
         }
+
     }
 }
