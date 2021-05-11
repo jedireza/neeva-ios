a14d1ccd333895b5e61d4c42762a6f207e672d91
Darin Fisher <79867437+darinwf@users.noreply.github.com>
2021-05-10 21:54:22 -0700

    Fix regression with backdrop color not specifying alpha channel (#291)
    The result without this change is a backdrop that is blue. Instead
    it should be 40% transparent black.

--- Client/Frontend/NeevaColors.swift
+++ Client/Frontend/NeevaColors.swift
@@ -23,6 +23,6 @@ extension UIColor {
         static let DarkElevated = UIColor(rgb: 0x3A3A3C)
         static let DefaultAqua = UIColor(rgb: 0x62B9F9)
         static let GlobeFavGray = UIColor(rgb: 0xB4BAC0)
-        static let Backdrop = UIColor(rgb: 0x00000066)  // Black + 40% transparency
+        static let Backdrop = UIColor(rgba: 0x00000066)  // Black + 40% transparency
     }
 }
