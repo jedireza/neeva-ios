105d11fd5310a4501b4c6bbc4fa6a94a97470db5
Evan Crow <evan@neeva.co>
2021-07-13 10:54:39 -0700

    Redesigned Download Panel (#950)
    * Redesigned download panel
    
    * Update Client/Frontend/DownloadsMenu/DownloadOverlayView.swift
    
    Co-authored-by: Jed Fox <76001228+jed-fox-neeva@users.noreply.github.com>
    
    * Update Client/Frontend/DownloadsMenu/DownloadOverlayView.swift
    
    Co-authored-by: Jed Fox <76001228+jed-fox-neeva@users.noreply.github.com>
    
    * Updated Truncation
    
    * Redesigned download panel
    
    * Redesigned download panel
    
    Update Client/Frontend/DownloadsMenu/DownloadOverlayView.swift
    
    Co-authored-by: Jed Fox <76001228+jed-fox-neeva@users.noreply.github.com>
    
    Update Client/Frontend/DownloadsMenu/DownloadOverlayView.swift
    
    Co-authored-by: Jed Fox <76001228+jed-fox-neeva@users.noreply.github.com>
    
    Updated Truncation
    
    * Update font weight
    
    * Update Client/Frontend/DownloadsMenu/DownloadViewController.swift
    
    Co-authored-by: Jed Fox <76001228+jed-fox-neeva@users.noreply.github.com>
    
    * Update Client/Frontend/Browser/OpenInHelper.swift
    
    Co-authored-by: Jed Fox <76001228+jed-fox-neeva@users.noreply.github.com>
    
    * Fixes
    
    * Color Change
    
    Co-authored-by: Jed Fox <76001228+jed-fox-neeva@users.noreply.github.com>

---
+++ Client/Frontend/DownloadsMenu/DownloadOverlayView.swift
@@ -0,0 +1,71 @@
+// Copyright Neeva. All rights reserved.
+
+import UIKit
+import SwiftUI
+import Shared
+
+struct DownloadMenuView: View {
+    let fileName: String
+    let fileURL: String
+    let fileSize: String?
+    
+    let onDownload: () -> ()
+    let onDismiss: () -> ()
+
+    private let cellHeight = CGFloat(52)
+    private struct Cell<Content: View>: View {
+        let content: () -> Content
+        var body: some View {
+            content()
+                .background(Color.background)
+                .cornerRadius(12)
+        }
+    }
+
+    public var body: some View {
+        VStack(alignment: .leading, spacing: 26) {
+            VStack(alignment: .leading, spacing: 4) {
+                Text(fileName)
+                    .withFont(.bodyLarge)
+                    .truncationMode(.middle)
+                    .foregroundColor(.primary)
+
+                Text(fileURL)
+                    .withFont(.labelMedium)
+                    .truncationMode(.head)
+                    .foregroundColor(.secondary)
+            }
+
+            VStack(alignment: .center, spacing: 12) {
+                Cell {
+                    Button(action: { onDownload() }) {
+                        HStack(alignment: .center) {
+                            Spacer()
+                            Text(fileSize != nil ? "Download (\(fileSize!))" : "Download")
+                            Spacer()
+                        }.frame(height: cellHeight)
+                    }
+                }
+
+                Cell {
+                    Button(action: { onDismiss() }) {
+                        HStack(alignment: .center) {
+                            Spacer()
+                            Text("Cancel").withFont(.labelLarge)
+                            Spacer()
+                        }.frame(height: cellHeight)
+                    }
+
+                }
+            }
+            .foregroundColor(.ui.adaptive.blue)
+            .buttonStyle(TableCellButtonStyle())
+        }.padding()
+    }
+}
+
+struct DownloadMenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        DownloadMenuView(fileName: "markus-spiske-jsqrSfrtjB80-unsplash.jpg", fileURL: "www.unsplash.com", fileSize: "2.2 MB", onDownload: {}, onDismiss: {})
+    }
+}
---
+++ Client/Frontend/DownloadsMenu/DownloadViewController.swift
@@ -0,0 +1,47 @@
+// Copyright Neeva. All rights reserved.
+
+import UIKit
+import SwiftUI
+
+struct DownloadRootView: View {
+    var overlaySheetModel = OverlaySheetModel()
+    let fileName: String
+    let fileURL: String
+    let fileSize: String?
+    
+    let onDownload: () -> ()
+    let onDismiss: () -> ()
+
+    var body: some View {
+        let config = OverlaySheetConfig(showTitle: false, backgroundColor: .systemGroupedBackground)
+        OverlaySheetView(model: self.overlaySheetModel, config: config, onDismiss: { onDismiss() }) {
+            DownloadMenuView(fileName: fileName, fileURL: fileURL, fileSize: fileSize, onDownload: { onDownload() }, onDismiss: { onDismiss() })
+                .overlaySheetIsFixedHeight(isFixedHeight: true)
+        }
+        .onAppear {
+            // It seems to be necessary to delay starting the animation until this point to
+            // avoid a visual artifact.
+            DispatchQueue.main.async {
+                self.overlaySheetModel.show()
+            }
+        }
+    }
+}
+
+class DownloadViewController: UIHostingController<DownloadRootView> {
+    init(fileName: String, fileURL: String, fileSize: String?, onDownload: @escaping () -> (), onDismiss: @escaping () -> ()) {
+        super.init(rootView: DownloadRootView(fileName: fileName, fileURL: fileURL, fileSize: fileSize, onDownload: onDownload, onDismiss: onDismiss))
+        self.view.accessibilityViewIsModal = true
+    }
+
+    @objc required dynamic init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    @objc override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // By default, a UIHostingController opens as an opaque layer, so we override
+        // that behavior here.
+        view.backgroundColor = .clear
+    }
+}
