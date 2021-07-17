dc371e0c6dde25561b076206303fa01e6d350a2e
Darin Fisher <darin@neeva.co>
2021-07-16 17:06:14 -0700

    refactor to share code

--- XCUITests/NeevaSignInTests.swift
+++ XCUITests/NeevaSignInTests.swift
@@ -24,15 +24,7 @@ class NeevaSignInTests: BaseTestCase {
         waitForValueContains(app.buttons["Address Bar"], value: withUrlContaining, timeout: 30.0)
     }
 
-    func testSignInFromPromoCard() {
-        XCTAssertNotNil(username)
-        XCTAssertNotNil(password)
-
-        waitForExistence(app.buttons["Sign in or Join Neeva"])
-        app.buttons["Sign in or Join Neeva"].tap()
-
-        waitUntilPageLoad(withUrlContaining: "https://neeva.com/signin")
-
+    fileprivate func doSignIn() {
         let textField = app.textFields.firstMatch
         XCTAssertEqual("Please enter your email address", textField.placeholderValue)
 
@@ -56,10 +48,12 @@ class NeevaSignInTests: BaseTestCase {
 
         waitUntilPageLoad(withUrlContaining: "https://neeva.com/")
 
+        // Expect first run dialog.
         waitForExistence(app.buttons["Got it!"])
         app.buttons["Got it!"].tap()
+    }
 
-        // Sign out
+    fileprivate func doSignOut() {
         navigator.goto(SettingsScreen)
 
         waitForExistence(app.cells["Member, \(username!)"])
@@ -85,6 +79,19 @@ class NeevaSignInTests: BaseTestCase {
         waitForExistence(app.webViews.links["Sign In"])
     }
 
+    func testSignInFromPromoCard() {
+        XCTAssertNotNil(username)
+        XCTAssertNotNil(password)
+
+        waitForExistence(app.buttons["Sign in or Join Neeva"])
+        app.buttons["Sign in or Join Neeva"].tap()
+
+        waitUntilPageLoad(withUrlContaining: "https://neeva.com/signin")
+
+        doSignIn()
+        doSignOut()
+    }
+
     func testSignInFromSettings() {
         XCTAssertNotNil(username)
         XCTAssertNotNil(password)
@@ -95,57 +102,9 @@ class NeevaSignInTests: BaseTestCase {
         app.cells["Sign In or Join Neeva"].tap()
 
         waitUntilPageLoad(withUrlContaining: "https://neeva.com/signin")
-
-        let textField = app.textFields.firstMatch
-        XCTAssertEqual("Please enter your email address", textField.placeholderValue)
-
-        UIPasteboard.general.string = username
-        textField.tap()
-        textField.press(forDuration: 2)
-        app.menus.firstMatch.menuItems["Paste"].tap()
-
-        waitForExistence(app.staticTexts["Sign in"])
-        app.staticTexts["Sign in"].tap()
-
-        waitUntilPageLoad(withUrlContaining: "https://login.neeva.com/")
-
-        // Password field should already be focused
-        UIPasteboard.general.string = password
-        app.secureTextFields.firstMatch.press(forDuration: 2)
-        app.menus.firstMatch.menuItems["Paste"].tap()
-
-        waitForExistence(app.buttons["Sign In"])
-        app.buttons["Sign In"].tap()
-
-        waitUntilPageLoad(withUrlContaining: "https://neeva.com/")
-
-        waitForExistence(app.buttons["Got it!"])
-        app.buttons["Got it!"].tap()
-
-        // Sign out
         navigator.nowAt(BrowserTab)
-        navigator.goto(SettingsScreen)
 
-        waitForExistence(app.cells["Member, \(username!)"])
-        app.cells["Member, \(username!)"].tap()
-
-        waitForExistence(app.buttons["Sign Out"])
-        app.buttons["Sign Out"].tap()
-
-        waitForExistence(app.sheets.firstMatch.staticTexts["Sign out of Neeva?"])
-        waitForExistence(app.sheets.firstMatch.buttons["Sign Out"])
-        app.sheets.firstMatch.buttons["Sign Out"].tap()
-
-        waitForExistence(app.cells["Sign In or Join Neeva"])
-
-        waitForExistence(app.navigationBars.buttons["Done"])
-        app.navigationBars.buttons["Done"].tap()
-
-        // Reload to ensure we are bounced to the marketing site.
-        waitForExistence(app.buttons["Reload"])
-        app.buttons["Reload"].tap()
-
-        waitUntilPageLoad(withUrlContaining: "https://neeva.com/")
-        waitForExistence(app.webViews.links["Sign In"])
+        doSignIn()
+        doSignOut()
     }
 }
