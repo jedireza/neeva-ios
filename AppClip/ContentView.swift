97c5e8d7122b25b73e00471b66c831ffd7b1e0a4
Evan Crow <evan@neeva.co>
2021-07-16 18:32:38 -0700

    Basic App Clip UI (#1009)
    * Created basic UI
    
    * Update ContentView.swift
    
    * Update AppClip/ContentView.swift
    
    Co-authored-by: Jed Fox <76001228+jed-fox-neeva@users.noreply.github.com>
    
    Co-authored-by: Jed Fox <76001228+jed-fox-neeva@users.noreply.github.com>

--- AppClip/ContentView.swift
+++ AppClip/ContentView.swift
@@ -1,11 +1,29 @@
 // Copyright Neeva. All rights reserved.
 
+import UIKit
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 16) {
+            HStack(spacing: 24) {
+                Image("neeva-logo")
+                Text("Neeva")
+                    .foregroundColor(Color("Blue"))
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+            }
+
+            Button(action: {
+                UIApplication.shared.open(URL(string: "https://apps.apple.com/us/app/neeva-browser-search-engine/id1543288638")!)
+            }, label: {
+                HStack {
+                    (Text("Download Now").underline().fontWeight(.semibold) +
+                        Text(" for a \nbetter search experience").foregroundColor(Color(UIColor.link))).multilineTextAlignment(.center)
+
+                }
+            })
+        }
     }
 }
 
