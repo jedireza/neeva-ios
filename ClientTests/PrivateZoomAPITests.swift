3bc4895e0780992972a918f6cd22708e12a34dc2
Jed Fox <76001228+jed-fox-neeva@users.noreply.github.com>
2021-07-27 17:14:25 -0400

    Add a basic test to verify the web view zoom API (#1116)

---
+++ ClientTests/PrivateZoomAPITests.swift
@@ -0,0 +1,18 @@
+// Copyright Neeva. All rights reserved.
+
+import XCTest
+import WebKit
+@testable import Client
+
+class PrivateZoomAPITests: XCTestCase {
+    let webView = WKWebView()
+
+    func testZoom() {
+        XCTAssertEqual(webView.neeva_zoomAmount, CGFloat(1))
+        let amounts: [CGFloat] = [3, 1, 0.5, 1.5, 0.25, 4 / 3]
+        for amount in amounts {
+            webView.neeva_zoomAmount = amount
+            XCTAssertEqual(webView.neeva_zoomAmount, amount)
+        }
+    }
+}
