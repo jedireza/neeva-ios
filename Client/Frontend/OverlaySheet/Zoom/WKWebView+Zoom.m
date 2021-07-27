ee17cfd8127cb9af4bdbf88cb66c8be6cd605ee0
Darin Fisher <79867437+darinwf@users.noreply.github.com>
2021-07-27 14:15:22 -0700

    Fix Zoom Page crash (#1110) (#1120)
    Co-authored-by: Jed Fox <76001228+jed-fox-neeva@users.noreply.github.com>

--- Client/Frontend/OverlaySheet/Zoom/WKWebView+Zoom.m
+++ Client/Frontend/OverlaySheet/Zoom/WKWebView+Zoom.m
@@ -6,17 +6,32 @@
 @implementation WKWebView (Zoom)
 
 - (CGFloat)neeva_zoomAmount {
+    char types[50];
+    sprintf(types, "%s%s%s", @encode(CGFloat), @encode(id), @encode(SEL));
+    NSMethodSignature *signature = [NSMethodSignature signatureWithObjCTypes:types];
+    NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:signature];
+    invocation.target = self;
     // "_viewScale"
     NSArray<NSString *> *chars = @[@"_vi", @"wScal", @""];
-    NSNumber *value = [self valueForKey:[chars componentsJoinedByString:@"e"]];
-    return [value doubleValue];
+    invocation.selector = NSSelectorFromString([chars componentsJoinedByString:@"e"]);
+    [invocation invoke];
+    CGFloat result;
+    [invocation getReturnValue:&result];
+    return result;
 }
 
 - (void)setNeeva_zoomAmount:(CGFloat)amount {
-    // "_viewScale"
-    unichar unichars[2] = {'w', 'S'};
-    NSArray<NSString *> *chars = @[@"_v", @"ie", [NSString stringWithCharacters:unichars length:2], @"ca", @"le"];
-    [self setValue:[NSNumber numberWithDouble:amount] forKey:[chars componentsJoinedByString:@""]];
+    char types[50];
+    sprintf(types, "%s%s%s%s", @encode(void), @encode(id), @encode(SEL), @encode(CGFloat));
+    NSMethodSignature *signature = [NSMethodSignature signatureWithObjCTypes:types];
+    NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:signature];
+    invocation.target = self;
+    // "_setViewScale:"
+    NSArray<NSString *> *chars = @[@"_s", @"tVi", @"wScal", @":"];
+    invocation.selector = NSSelectorFromString([chars componentsJoinedByString:@"e"]);
+    // Implicit arguments are 0 => self, 1 => @selector(_setViewScale:)
+    [invocation setArgument:&amount atIndex:2];
+    [invocation invoke];
 }
 
 @end
