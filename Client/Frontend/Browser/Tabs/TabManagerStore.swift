jedireza/neeva-ios

commit 30a07d11196dbee20960f217f38cdc917998f6c9

Enable Empty Tab Tray (#1515)

Client/Frontend/Browser/Tabs/TabManagerStore.swift
@@ -11,9 +11,9 @@ private let log = Logger.storage
 
 class TabManagerStore {
     static let shared = TabManagerStore(
-            imageStore: DiskImageStore(
-                files: getAppDelegateProfile().files, namespace: "TabManagerScreenshots",
-                quality: UIConstants.ScreenshotQuality))
+        imageStore: DiskImageStore(
+            files: getAppDelegateProfile().files, namespace: "TabManagerScreenshots",
+            quality: UIConstants.ScreenshotQuality))
 
     fileprivate var lockedForReading = false
     public let imageStore: DiskImageStore?
@@ -89,7 +89,7 @@ class TabManagerStore {
         assert(Thread.isMainThread)
 
         guard let savedTabs = prepareSavedTabs(fromTabs: tabs, selectedTab: selectedTab),
-              let path = tabSavePath(withId: scene.session.persistentIdentifier)
+            let path = tabSavePath(withId: scene.session.persistentIdentifier)
         else {
             clearArchive(for: scene)
             return succeed()
@@ -159,7 +159,7 @@ class TabManagerStore {
         for savedTab in savedTabs {
             // Provide an empty request to prevent a new tab from loading the home screen
             var tab = tabManager.addTab(
-                flushToDisk: false, zombie: true, isPrivate: savedTab.isPrivate)
+                flushToDisk: true, zombie: true, isPrivate: savedTab.isPrivate)
             tab = savedTab.configureSavedTabUsing(tab, imageStore: imageStore)
 
             if savedTab.isSelected {
@@ -168,7 +168,11 @@ class TabManagerStore {
         }
 
         if tabToSelect == nil {
-            tabToSelect = tabManager.tabs.first(where: { $0.isIncognito == false })
+            if !tabManager.normalTabs.isEmpty {
+                tabToSelect = tabManager.tabs.first(where: { $0.isIncognito == false })
+            } else {
+                SceneDelegate.getBVC(with: tabManager.scene).showTabTray()
+            }
         }
 
         return tabToSelect
@@ -215,8 +219,9 @@ class TabManagerStore {
 extension TabManagerStore {
     func testTabCountOnDisk(sceneId: String) -> Int {
         assert(AppConstants.IsRunningTest)
-        return SiteArchiver.tabsToRestore(
-            tabsStateArchivePath: tabSavePath(withId: sceneId)
-        )?.count ?? 0
+        return
+            SiteArchiver.tabsToRestore(
+                tabsStateArchivePath: tabSavePath(withId: sceneId)
+            )?.count ?? 0
     }
 }

Client.xcodeproj/project.pbxproj
@@ -182,6 +182,8 @@
 		66430430267C19D200674549 /* ToastDefaults.swift in Sources */ = {isa = PBXBuildFile; fileRef = 6643042F267C19D200674549 /* ToastDefaults.swift */; };
 		664825F0268FD106000E7F6A /* XCUIElementExtension.swift in Sources */ = {isa = PBXBuildFile; fileRef = 664825EF268FD106000E7F6A /* XCUIElementExtension.swift */; };
 		665879CE26C3408700B53EB7 /* WindowManager.swift in Sources */ = {isa = PBXBuildFile; fileRef = 665879CD26C3408700B53EB7 /* WindowManager.swift */; };
+		6664890726D9829200A4C493 /* EmptyTabTrayTests.swift in Sources */ = {isa = PBXBuildFile; fileRef = 6664890626D9829200A4C493 /* EmptyTabTrayTests.swift */; };
+		6666D8C726D40278001AD43F /* LazyTabTests.swift in Sources */ = {isa = PBXBuildFile; fileRef = 6666D8C626D40278001AD43F /* LazyTabTests.swift */; };
 		666CFD8A26C735DC009ED9CF /* EmptyCardGrid.swift in Sources */ = {isa = PBXBuildFile; fileRef = 666CFD8926C735DC009ED9CF /* EmptyCardGrid.swift */; };
 		6670187126BB678700960CF3 /* SuggestionModel.swift in Sources */ = {isa = PBXBuildFile; fileRef = 6670187026BB678700960CF3 /* SuggestionModel.swift */; };
 		667DB63A2698E3DF0054A222 /* KeyboardShortcutTests.swift in Sources */ = {isa = PBXBuildFile; fileRef = 66E2ED882697C3B400E01010 /* KeyboardShortcutTests.swift */; };
@@ -1133,6 +1135,7 @@
 		664825EF268FD106000E7F6A /* XCUIElementExtension.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = XCUIElementExtension.swift; sourceTree = "<group>"; };
 		665879CD26C3408700B53EB7 /* WindowManager.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = WindowManager.swift; sourceTree = "<group>"; };
 		665CE64726DDA7E70069EFDD /* FeedbackRequest.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = FeedbackRequest.swift; sourceTree = "<group>"; };
+		6664890626D9829200A4C493 /* EmptyTabTrayTests.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = EmptyTabTrayTests.swift; sourceTree = "<group>"; };
 		6666D8C626D40278001AD43F /* LazyTabTests.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = LazyTabTests.swift; sourceTree = "<group>"; };
 		666CFD8926C735DC009ED9CF /* EmptyCardGrid.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = EmptyCardGrid.swift; sourceTree = "<group>"; };
 		6670187026BB678700960CF3 /* SuggestionModel.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SuggestionModel.swift; sourceTree = "<group>"; };
@@ -2114,6 +2117,7 @@
 				2C473BCF209778900008C853 /* DownloadFilesTests.swift */,
 				2C4A07DB20246EAD0083E320 /* DragAndDropTests.swift */,
 				66E26DB026E2F80200EB23D6 /* EditURLTests.swift */,
+				6664890626D9829200A4C493 /* EmptyTabTrayTests.swift */,
 				2C8C07761E7800EA00DC1237 /* FindInPageTest.swift */,
 				2C2A91281FA2410D002E36BD /* HistoryTests.swift */,
 				3BFE4B0B1D342FB900DDF53F /* Info.plist */,
@@ -4492,6 +4496,8 @@
 				C832BF7426E0B31B00B9A1F8 /* SettingsTest.swift in Sources */,
 				C832BF7926E0B31B00B9A1F8 /* ZeroQueryTests.swift in Sources */,
 				C832BF7726E0B31B00B9A1F8 /* TabMenuTests.swift in Sources */,
+				6664890726D9829200A4C493 /* EmptyTabTrayTests.swift in Sources */,
+				3D9CAA1C1EFCD655002434DD /* ClipBoardTests.swift in Sources */,
 				6618B51C26AB3CBD00ED51C9 /* BaseTestCase+GoToExtension.swift in Sources */,
 				C832BF7526E0B31B00B9A1F8 /* ToastTests.swift in Sources */,
 				C832BF6F26E0B31500B9A1F8 /* DownloadFilesTests.swift in Sources */,

Client/Application/TestAppDelegate.swift
@@ -163,6 +163,12 @@ class TestAppDelegate: AppDelegate {
             Defaults[.introSeen] = true
         }
 
+        if !launchArguments.contains(LaunchArguments.DontAddTabOnLaunch) {
+            Defaults[.createNewTabOnStart] = true
+        } else {
+            Defaults[.createNewTabOnStart] = false
+        }
+
         // Deferred to here in case the ClearProfile argument was set.
         if let loginCookie = loginCookie {
             NeevaUserInfo.shared.setLoginCookie(loginCookie)

Client/Frontend/Browser/BrowserViewController.swift
@@ -536,7 +536,13 @@ class BrowserViewController: UIViewController {
             displayedRestoreTabsAlert = true
             showRestoreTabsAlert()
         } else {
-            tabManager.restoreTabs()
+            if !tabManager.restoreTabs() {
+                if Defaults[.createNewTabOnStart] {
+                    tabManager.select(tabManager.addTab())
+                } else {
+                    showTabTray()
+                }
+            }
         }
 
         clipboardBarDisplayHandler?.checkIfShouldDisplayBar()
@@ -561,7 +567,10 @@ class BrowserViewController: UIViewController {
         let alert = UIAlertController.restoreTabsAlert(
             okayCallback: { _ in
                 self.isCrashAlertShowing = false
-                self.tabManager.restoreTabs(true)
+
+                if !self.tabManager.restoreTabs(true) {
+                    self.showTabTray()
+                }
             },
             noCallback: { _ in
                 self.isCrashAlertShowing = false
@@ -684,11 +693,11 @@ class BrowserViewController: UIViewController {
         guard zeroQueryModel.openedFrom == nil else { return }
 
         if !cardGridViewController.gridModel.isHidden {
-            cardGridViewController.gridModel.hideWithNoAnimation()
+            hideCardGrid(withAnimation: false)
         }
 
         if isLazyTab {
-            chromeModel.setEditingLocation(to: true)
+            chromeModel.triggerOverlay()
         }
 
         searchQueryModel.value = ""
@@ -909,7 +918,10 @@ class BrowserViewController: UIViewController {
             request = nil
         }
 
-        tabManager.selectTab(tabManager.addTab(request, isPrivate: isPrivate))
+        DispatchQueue.main.async {
+            self.tabManager.selectTab(self.tabManager.addTab(request, isPrivate: isPrivate))
+            self.cardGridViewController.gridModel.hideWithNoAnimation()
+        }
     }
 
     func openURLInNewTabPreservingIncognitoState(_ url: URL) {
@@ -1131,6 +1143,14 @@ class BrowserViewController: UIViewController {
 
         if let tab = tabManager.selectedTab {
             screenshotHelper.takeScreenshot(tab)
+        }
+    }
+
+    func hideCardGrid(withAnimation: Bool) {
+        if withAnimation {
+            cardGridViewController.gridModel.hideWithAnimation()
+        } else {
+            cardGridViewController.gridModel.hideWithNoAnimation()
         }
     }
 }
@@ -1552,26 +1572,27 @@ extension BrowserViewController {
         introViewController!.visitHomePage = visitHomePage
         introViewController!.visitSigninPage = visitSigninPage
         introViewController!.visitAppleAuthPage = visitAppleAuthPage(authURL:)
+        introViewController!.skipToBrowser = skipToBrowser
 
         self.introVCPresentHelper(introViewController: introViewController!)
     }
 
     private func visitHomePage() {
-        if let tab = self.tabManager.selectedTab {
-            tab.loadRequest(URLRequest(url: NeevaConstants.appSignupURL))
-        }
+        openURLInNewTab(NeevaConstants.appSignupURL)
     }
 
     private func visitSigninPage() {
-        if let tab = self.tabManager.selectedTab {
-            tab.loadRequest(URLRequest(url: NeevaConstants.appSigninURL))
-        }
+        openURLInNewTab(NeevaConstants.appSigninURL)
     }
 
     private func visitAppleAuthPage(authURL: URL) {
         if let tab = self.tabManager.selectedTab {
             tab.loadRequest(URLRequest(url: authURL))
         }
+    }
+
+    private func skipToBrowser() {
+        openURLInNewTab(URL(string: "https://neeva.com"))
     }
 
     private func introVCPresentHelper(introViewController: UIViewController) {

Client/Frontend/Browser/BrowserViewController/BrowserViewController+TabToolbarDelegate.swift
@@ -123,7 +123,7 @@ extension BrowserViewController: ToolbarDelegate {
                     title: Strings.CloseTabTitle, image: UIImage(systemSymbol: .xmark)
                 ) { _ in
                     if let tab = self.tabManager.selectedTab {
-                        self.tabManager.removeTabAndUpdateSelectedTab(tab, addNewTab: true)
+                        self.tabManager.removeTabAndUpdateSelectedTab(tab)
                     }
                 }
                 closeTabAction.accessibilityIdentifier = "Close Tab Action"

Client/Frontend/Browser/BrowserViewController/BrowserViewController+WebViewDelegates.swift
@@ -203,7 +203,7 @@ extension BrowserViewController: WKUIDelegate {
         if let tab = tabManager[webView] {
             // Need to wait here in case we're waiting for a pending `window.open()`.
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                self.tabManager.removeTabAndUpdateSelectedTab(tab, addNewTab: true)
+                self.tabManager.removeTabAndUpdateSelectedTab(tab)
             }
         }
     }
@@ -259,7 +259,8 @@ extension BrowserViewController: WKUIDelegate {
                                 buttonText: Strings.ContextMenuButtonToastNewTabOpenedButtonText,
                                 buttonAction: {
                                     self.tabManager.selectTab(tab)
-                                }).enqueue(manager: toastManager)
+                                }
+                            ).enqueue(manager: toastManager)
                         }
                     }
 

Client/Frontend/Browser/Card/BrowserPrimitive.swift
@@ -117,7 +117,7 @@ extension TabManager: ClosingManager, SelectingManager, AccessingManager {
 
     func close(_ tab: Tab) {
         withAnimation {
-            removeTabAndUpdateSelectedTab(tab, allowToast: true, addNewTab: false)
+            removeTabAndUpdateSelectedTab(tab, allowToast: true)
         }
     }
 

Client/Frontend/Browser/Card/CardGrid.swift
@@ -93,6 +93,7 @@ struct CardGrid: View {
     @ViewBuilder var cardContainer: some View {
         if tabModel.isCardGridEmpty {
             EmptyCardGrid()
+                .accessibility(identifier: "Empty Card Grid")
         } else {
             VStack(spacing: 0) {
                 CardsContainer(

commit 08ec672cac1d957dd4bb674e33b7d32107045cfe

Dont flush to disk immediately when we create restored tabs (#1672)
This was a regression from #1515 where this parametere turned to true from false

Client/Frontend/Browser/Tabs/TabManagerStore.swift
@@ -155,7 +155,7 @@ class TabManagerStore {
         for savedTab in savedTabs {
             // Provide an empty request to prevent a new tab from loading the home screen
             var tab = tabManager.addTab(
-                flushToDisk: true, zombie: true, isPrivate: savedTab.isPrivate)
+                flushToDisk: false, zombie: true, isPrivate: savedTab.isPrivate)
             tab = savedTab.configureSavedTabUsing(tab, imageStore: imageStore)
 
             if savedTab.isSelected {