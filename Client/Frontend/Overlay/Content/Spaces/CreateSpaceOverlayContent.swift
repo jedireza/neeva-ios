e3658ccc95171bf678afc5e9eb2302ddef0d793c
Evan Crow <evan@neeva.co>
2021-10-12 20:58:00 -0700

    Fix Space Animation (#1950)
    * Fix Share Space Animation
    
    * Update AddToSpaceOverlayContent.swift

--- Client/Frontend/Overlay/Content/Spaces/CreateSpaceOverlayContent.swift
+++ Client/Frontend/Overlay/Content/Spaces/CreateSpaceOverlayContent.swift
@@ -24,7 +24,6 @@ struct CreateSpaceOverlayContent: View {
                                     spaceModel.allDetails.first?.isShowingDetails = true
                                     subscription?.cancel()
                                 }
-
                             }
                         }
                     case .failure:
