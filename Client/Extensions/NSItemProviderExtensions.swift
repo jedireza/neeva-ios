7c4d53e1d31d496afd0d6229741e273867b65f2d
Evan Crow <evan@neeva.co>
2021-10-20 21:08:26 -0700

    Edit Space List Item Fix (#2003)

--- Client/Extensions/NSItemProviderExtensions.swift
+++ Client/Extensions/NSItemProviderExtensions.swift
@@ -6,4 +6,8 @@ extension NSItemProvider {
     convenience init(url: URL) {
         self.init(object: url as NSURL)
     }
+
+    convenience init(id: String) {
+        self.init(object: id as NSString)
+    }
 }
