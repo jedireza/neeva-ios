45a749bb0e1e47e069adc8f9f5d2a59d8d3f0391
Evan Crow <evan@neeva.co>
2021-10-29 17:30:23 -0700

    Fix Neeva Toggle State (iOS Safari Extension) (#2099)
    * Fix Neeva Toggle State
    
    * Update popup.js

--- NeevaForSafari/NeevaForSafariExtension/Resources/popup.js
+++ NeevaForSafari/NeevaForSafariExtension/Resources/popup.js
@@ -6,9 +6,11 @@ document.getElementById("navigateToNeevaButton").onclick = function() {
     window.open("https://neeva.com");
 };
 
-document.getElementById("downloadNeevaAppButton").onclick = function() {
-    window.open("https://apps.apple.com/us/app/neeva-browser-search-engine/id1543288638");
-};
+// This code was used to open the Neeva app store page when we had a button for it in the extension.
+// Leaving it here in case we need it sometime in the future.
+// document.getElementById("downloadNeevaAppButton").onclick = function() {
+//     window.open("https://apps.apple.com/us/app/neeva-browser-search-engine/id1543288638");
+// };
 
 browser.runtime.sendMessage({ "getPreference": "neevaRedirect"}).then((response) => {
     document.getElementById("neevaRedirectToggle").checked = response["value"]
