8c52a93df86044e871c441f7d14fed8d3eb195ca
macy <macy@neeva.co>
2021-11-05 21:33:00 -0700

    [ios] rename project from Client to Neeva

---
+++ Neeva/Application/Client-Bridging-Header.h
@@ -0,0 +1,19 @@
+// Copyright Neeva. All rights reserved.
+
+#ifndef Client_Bridging_Header_h
+#define Client_Bridging_Header_h
+
+#import <Foundation/Foundation.h>
+#import <CommonCrypto/CommonCrypto.h>
+
+#import "ThirdParty/UIImageViewAligned/UIImageViewAligned.h"
+#import "ThirdParty/Apple/UIImage+ImageEffects.h"
+#import "ThirdParty/KMCGeigerCounter/KMCGeigerCounter/KMCGeigerCounter.h"
+
+#import "Shared-Bridging-Header.h"
+#import "Storage-Bridging-Header.h"
+
+#import <WebKit/WebKit.h>
+#import "WKWebView+Zoom.h"
+
+#endif /* Client_Bridging_Header_h */
