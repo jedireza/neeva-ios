3e6ff44e83d27b6e5b9d91efdd8a7afd2ee32c10
macy-n <61707598+macy-n@users.noreply.github.com>
2021-12-01 20:11:20 -0800

    [ios] add back Neeva feature flag (#2368)

--- Shared/NeevaFeatureFlags.swift
+++ Shared/NeevaFeatureFlags.swift
@@ -50,6 +50,7 @@ public class NeevaFeatureFlags {
     // that mirror the values defined in //neeva/serving/featureflags/data/.
 
     public enum BoolFlag: Int, CaseIterable {
+        case clientHideSearchBoxOnAllPagesAndMoveFilters = 54750
         case browserQuests = 42234
         case suggestionsLogging = 45278
         case neevaMemory = 40640
@@ -72,6 +73,8 @@ public class NeevaFeatureFlags {
 
         public var name: String {
             switch self {
+            case .clientHideSearchBoxOnAllPagesAndMoveFilters:
+                return "client.hide_search_bar_on_all_pages_and_move_filters"
             case .browserQuests: return "ios.quests"
             case .suggestionsLogging: return "ios.log.suggestions"
             case .neevaMemory: return "privacy.frontend_options"
