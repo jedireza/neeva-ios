d40ae9ca32dba05e9d33d1a031082764b00f6ade
macy <macy@neeva.co>
2021-12-04 18:52:37 -0800

    update

--- Client/Frontend/CryptoWallet/WalletDashboard.swift
+++ Client/Frontend/CryptoWallet/WalletDashboard.swift
@@ -64,7 +64,7 @@ struct WalletDashboard: View {
 
         }
         .padding(.top, 10)
-        .padding(.vertical, 25)
+        .padding(.horizontal, 25)
         .onAppear(perform: getData)
     }
 
