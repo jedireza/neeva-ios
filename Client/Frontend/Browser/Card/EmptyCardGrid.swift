8582ead1cb4dfcfb7691c30ab44d686129d27bf1
Evan Crow <evan@neeva.co>
2021-12-10 17:30:57 -0800

    Right color this time :) (#2418)

--- Client/Frontend/Browser/Card/EmptyCardGrid.swift
+++ Client/Frontend/Browser/Card/EmptyCardGrid.swift
@@ -14,7 +14,7 @@ struct EmptyCardGrid: View {
                 .withFont(.bodyMedium)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.DefaultBackground.ignoresSafeArea())
+        .background(Color.background.ignoresSafeArea())
     }
 }
 
