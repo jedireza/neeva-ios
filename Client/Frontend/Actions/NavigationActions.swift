0b5253e0357d671103b0b0d13d6b656882672ca1
Jed Fox <jed@neeva.co>
2022-01-06 16:04:44 -0500

    NavigationActions
    This is a bad idea because there are not that many of these actions, apparently

---
+++ Client/Frontend/Actions/NavigationActions.swift
@@ -0,0 +1,10 @@
+// Copyright Neeva. All rights reserved.
+
+import Foundation
+
+struct NavigationActions: ActionProvider {
+    private(set) lazy var openSwitcher = { /* TODO */ }
+    private(set) lazy var openSwitcherAnimated = { /* TODO */ }
+    private(set) lazy var openSwitcher = { /* TODO */ }
+    private(set) lazy var showSpaces = { }
+}
