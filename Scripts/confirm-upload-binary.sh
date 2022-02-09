ef78fd20f3276d72190b51a46c1f136b25f53b1a
chung-lai <87332578+chung-lai@users.noreply.github.com>
2022-02-08 18:33:41 -0800

    Remove slack script from build (#2827)
    * Remove slack script from build
    
    * Code review feedback

--- Scripts/confirm-upload-binary.sh
+++ Scripts/confirm-upload-binary.sh
@@ -13,4 +13,13 @@ subtitle="<https://appstoreconnect.apple.com/apps/1543288638/testflight/ios/|App
 echo "Build uploaded to App Store? Press ENTER to continue. Ctrl+C to cancel."
 read
 
-Scripts/send-slack-message.sh "$title" "$subtitle"
+if test -f "$NEEVA_REPO/client/browser/scripts/send-slack-message.sh"; then
+  $NEEVA_REPO/client/browser/scripts/send-slack-message.sh "$title" "$subtitle"
+else
+  if [ -z ${NEEVA_REPO} ]; then
+    echo 'Set $NEEVA_REPO to point to your neeva main repository'
+  else
+    echo "No slack script detected, make sure you have the latest changes from main Neeva repo"
+  fi
+  exit 1
+fi
