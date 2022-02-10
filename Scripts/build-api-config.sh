4de184ca119da8d763c4c42c5b4e48d03167d5ee
chung-lai <87332578+chung-lai@users.noreply.github.com>
2022-02-10 13:22:37 -0800

    Use literal pattern instead of regex to replace string so we don't have to decode url path for regex (#2844)

--- Scripts/build-api-config.sh
+++ Scripts/build-api-config.sh
@@ -5,7 +5,7 @@
 echo "Building API Config"
 
 cp Client/APIConfig-sample.xcconfig Client/APIConfig.xcconfig
-sed -i '' "s/\$OPENSEA_API_KEY/$OPENSEA_API_KEY/g" Client/APIConfig.xcconfig
-sed -i '' "s/\$CRYPTO_ETH_URL/$CRYPTO_ETH_URL/g" Client/APIConfig.xcconfig
-sed -i '' "s/\$CRYPTO_ROPSTEN_URL/$CRYPTO_ROPSTEN_URL/g" Client/APIConfig.xcconfig
-sed -i '' "s/\$CRYPTO_POLYGON_URL/$CRYPTO_POLYGON_URL/g" Client/APIConfig.xcconfig
+sed -i '' "s|\$OPENSEA_API_KEY|$OPENSEA_API_KEY|g" Client/APIConfig.xcconfig
+sed -i '' "s|\$CRYPTO_ETH_URL|$CRYPTO_ETH_URL|g" Client/APIConfig.xcconfig
+sed -i '' "s|\$CRYPTO_ROPSTEN_URL|$CRYPTO_ROPSTEN_URL|g" Client/APIConfig.xcconfig
+sed -i '' "s|\$CRYPTO_POLYGON_URL|$CRYPTO_POLYGON_URL|g" Client/APIConfig.xcconfig
