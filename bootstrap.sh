861524fe66440ffd2a9abf640094117edce7e697
Darin Fisher <79867437+darinwf@users.noreply.github.com>
2022-03-24 14:24:39 -0700

    Enable simulator support on an M1 mac (#3061)

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,37 +10,11 @@
 # CI services where the Carthage directory can be cached.
 #
 # Use the --force option to force a rebuild of the dependencies.
-# Use the --importLocales option to fetch and update locales only
 #
 
-getLocale()
-{
-  echo "Getting locale..."
-  git clone https://github.com/boek/ios-l10n-scripts.git -b new_tool || exit 1
-
-  echo "Creating firefoxios-l10n Git repo"
-  rm -rf firefoxios-l10n
-  git clone --depth 1 https://github.com/mozilla-l10n/firefoxios-l10n firefoxios-l10n || exit 1
-}
-
 if [ "$1" == "--force" ]; then
-    rm -rf firefoxios-l10n
-    rm -rf ios-l10n-scripts
     rm -rf Carthage/*
     rm -rf ~/Library/Caches/org.carthage.CarthageKit
-fi
-
-if [ "$1" == "--importLocales" ]; then
-  # Import locales
-  if [ -d "/firefoxios-l10n" ] && [ -d "/ios-l10n-scripts" ]; then
-      echo "l10n directories found. Not downloading scripts."
-  else
-      echo "l10n directory not found. Downloading repo and scripts."
-      getLocale
-  fi
-
-  ./ios-l10n-scripts/ios-l10n-tools --project-path Client.xcodeproj --l10n-project-path ./firefoxios-l10n --import
-  exit 0
 fi
 
 # Build API Config
