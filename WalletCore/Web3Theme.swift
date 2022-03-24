aa982f5ddfa49e4695aabc73c5115be1e8f4c4b3
yozuysal <83613310+yozuysal@users.noreply.github.com>
2022-03-24 10:52:45 -0700

    Fix copyright text for Web3Theme (#3201)
    Co-authored-by: Burak Üstün <burak@neeva.co>

--- WalletCore/Web3Theme.swift
+++ WalletCore/Web3Theme.swift
@@ -1,10 +1,6 @@
-//
-//  Web3Themes.swift
-//  WalletCore
-//
-//  Created by Burak Üstün on 22.03.2022.
-//  Copyright © 2022 Neeva. All rights reserved.
-//
+// Copyright 2022 Neeva Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 import Defaults
 import SDWebImageSwiftUI
